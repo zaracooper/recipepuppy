f3ae9ce33c2abcda8ff140c582573ca12fe8ea57
Zara Cooper <zramcooper@gmail.com>
2020-03-11 12:39:33 +0300

    Fix buggy API

--- v2/api.go
+++ v2/api.go
@@ -5,90 +5,99 @@ import (
 	"errors"
 	"io/ioutil"
 	"net/http"
-	"net/url"
 	"strings"
 )
 
-// Recipe is a recipe for a meal
+// Recipe is a recipe for a meal.
 type Recipe struct {
 	Title       string `json:"title,omitempty"`
 	Link        string `json:"href,omitempty"`
-	Ingredients string `json:"ingredients,omitempty"`
 	Picture     string `json:"thumbnail,omitempty"`
+	Ingredients []string
+}
+
+type recipeResponse struct {
+	Recipe
+	IngredientString string `json:"ingredients,omitempty"`
 }
 
 type response struct {
-	Title   string   `json:"title,omitempty"`
-	Version float64  `json:"version,omitempty"`
-	Link    string   `json:"href,omitempty"`
-	Recipes []Recipe `json:"results,omitempty"`
+	Title   string           `json:"title,omitempty"`
+	Version float64          `json:"version,omitempty"`
+	Link    string           `json:"href,omitempty"`
+	Recipes []recipeResponse `json:"results,omitempty"`
 }
 
-// APIHREF is the HREF of the Recipe Puppy API
+// APIHREF is the HREF of the Recipe Puppy API.
 const APIHREF = "http://recipepuppy.com/api"
 
-// FindRecipes finds recipes that match the recipe titles provided. The API returns
+// FindRecipes finds recipes that match the recipe title provided. The API returns
 // 10 results at a time. So use page to specify what page of results to fecth.
-func FindRecipes(recipeTitles []string, page int) ([]Recipe, error) {
-	if isQueryBlank(recipeTitles) || page == 0 {
-		return []Recipe{}, errors.New("Recipe titles cannot be blank. Page cannot be zero")
-	}
-
-	results := response{}
-
-	err := makeRequest(url.Values{"q": recipeTitles}, &results)
-	if err != nil {
-		return nil, err
-	}
-
-	return results.Recipes, nil
+func FindRecipes(recipeTitle string, page int) ([]Recipe, error) {
+	return processQuery(true, false, recipeTitle, []string{}, page, "Recipe title cannot be blank. Page cannot be zero")
 }
 
-// FindRecipesWithIngredients finds recipes that match the recipe titles and ingredients provided. The API returns
+// FindRecipesWithIngredients finds recipes that match the recipe title and ingredients provided. The API returns
 // 10 results at a time. So use page to specify what page of results to fecth.
-func FindRecipesWithIngredients(recipeTitles []string, ingredients []string, page int) ([]Recipe, error) {
-	if isQueryBlank(recipeTitles) || isQueryBlank(ingredients) || page == 0 {
-		return []Recipe{}, errors.New("Recipe titles or ingredients cannot be blank. Page cannot be zero")
-	}
-
-	results := response{}
-
-	err := makeRequest(url.Values{"q": recipeTitles, "i": ingredients}, &results)
-	if err != nil {
-		return nil, err
-	}
-
-	return results.Recipes, nil
+func FindRecipesWithIngredients(recipeTitle string, ingredients []string, page int) ([]Recipe, error) {
+	return processQuery(true, true, recipeTitle, ingredients, page, "Recipe title or ingredients cannot be blank. Page cannot be zero")
 }
 
 // FindRecipesByIngredients finds recipes that use the provided ingredient. The API returns
 // 10 results at a time. So use page to specify what page of results to fecth.
 func FindRecipesByIngredients(ingredients []string, page int) ([]Recipe, error) {
-	if isQueryBlank(ingredients) || page == 0 {
-		return []Recipe{}, errors.New("Ingredient cannot be blank. Page cannot be zero")
+	return processQuery(false, true, "", ingredients, page, "Ingredient cannot be blank. Page cannot be zero")
+}
+
+func unpackRecipes(resp response) []Recipe {
+	recipeResp := recipeResponse{}
+	unpackedRecipes := []Recipe{}
+
+	for i := 0; i < len(resp.Recipes); i++ {
+		recipeResp = resp.Recipes[i]
+		recipeResp.Title = strings.TrimSpace(recipeResp.Title)
+		recipeResp.Ingredients = strings.Split(recipeResp.IngredientString, ", ")
+		unpackedRecipes = append(unpackedRecipes, recipeResp.Recipe)
+	}
+
+	return unpackedRecipes
+}
+
+func processQuery(searchRecipe, searchIngr bool, recipeTitle string, ingredients []string, page int, errString string) ([]Recipe, error) {
+	if len(strings.TrimSpace(recipeTitle)) == 0 && searchRecipe || len(strings.TrimSpace(strings.Join(ingredients, ""))) == 0 && searchIngr || page == 0 {
+		return []Recipe{}, errors.New(errString)
+	}
+
+	var queryVal string
+
+	if searchRecipe {
+		queryVal = "q=" + recipeTitle
+		if searchIngr {
+			queryVal += "&"
+		}
+	}
+
+	if searchIngr {
+		queryVal += ("i=" + strings.Join(ingredients, ","))
 	}
 
 	results := response{}
 
-	err := makeRequest(url.Values{"i": ingredients}, &results)
+	err := makeRequest(queryVal, &results)
 	if err != nil {
 		return nil, err
 	}
 
-	return results.Recipes, nil
+	return unpackRecipes(results), nil
 }
 
-func isQueryBlank(query []string) bool {
-	return len(strings.TrimSpace(strings.Join(query, ""))) == 0
-}
-
-func makeRequest(query url.Values, results interface{}) error {
+func makeRequest(query string, results interface{}) error {
 	req, err := http.NewRequest("GET", APIHREF, nil)
 	if err != nil {
 		return err
 	}
 
-	req.URL.RawQuery = query.Encode()
+	req.URL.RawQuery = query
 
 	client := http.Client{}
 
